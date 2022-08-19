26b50754c8a7f624fc64ffdbd347da99e8660f80
hrs <1410722757@qq.com>
2022-08-19 19:44:16 +0800

    一些更改

--- time.py
+++ time.py
@@ -1,8 +1,12 @@
 import time
+from turtle import forward
 import torch 
 from torch import nn
 
 class ShuffleNet(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.int = 1
+        self.conv1 = nn.Conv2d(3,64)
+    
+    def forward(self, x):
+        return 0
